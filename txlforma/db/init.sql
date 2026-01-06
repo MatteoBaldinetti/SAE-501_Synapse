01495bfc41c9c4b9f91d08afdaf81db4abb704a6
Ken DINH <ken.dinh@edu.univ-eiffel.fr>
2026-01-06 17:56:26 +0100

    Updated the initial database

--- txlforma/db/init.sql
+++ txlforma/db/init.sql
@@ -39,6 +39,7 @@ CREATE TABLE `file` (
 
 LOCK TABLES `file` WRITE;
 /*!40000 ALTER TABLE `file` DISABLE KEYS */;
+INSERT INTO `file` VALUES (1,'1767199048837-fernando-alonso-aston-martin-r.jpg','2025-12-31 16:37:28.840887','/api/files/download/1767199048837-fernando-alonso-aston-martin-r.jpg'),(2,'1767621182345-kirby-pictures-6f3961bh9tvmntwq.jpg','2026-01-05 13:53:02.350071','/api/files/download/1767621182345-kirby-pictures-6f3961bh9tvmntwq.jpg'),(3,'1767684132665-slider-programme-dev-web.jpeg','2026-01-06 07:22:12.666484','/api/files/download/1767684132665-slider-programme-dev-web.jpeg'),(4,'1767684665621-101_UX_vs_UI_illustration_blog-1.png','2026-01-06 07:31:05.621991','/api/files/download/1767684665621-101_UX_vs_UI_illustration_blog-1.png'),(5,'1767684747798-MicrosoftTeams-image-10.jpg','2026-01-06 07:32:27.800581','/api/files/download/1767684747798-MicrosoftTeams-image-10.jpg'),(6,'1767687824225-kirby-pictures-6f3961bh9tvmntwq.jpg','2026-01-06 08:23:44.226418','/api/files/download/1767687824225-kirby-pictures-6f3961bh9tvmntwq.jpg'),(7,'1767688034832-kirby-pictures-6f3961bh9tvmntwq.jpg','2026-01-06 08:27:14.834319','/api/files/download/1767688034832-kirby-pictures-6f3961bh9tvmntwq.jpg'),(52,'1767717058580-admin-panel.png','2026-01-06 16:30:58.608714','/api/files/download/1767717058580-admin-panel.png'),(53,'1767717915141-happy-young-man-laughing_23-2148911860.avif','2026-01-06 16:45:15.143672','/api/files/download/1767717915141-happy-young-man-laughing_23-2148911860.avif'),(54,'1767717969329-premium_photo-1689551670902-19b441a6afde.jpg','2026-01-06 16:46:09.335754','/api/files/download/1767717969329-premium_photo-1689551670902-19b441a6afde.jpg'),(55,'1767718018321-selfie-portrait-videocall_23-2149186122.avif','2026-01-06 16:46:58.321940','/api/files/download/1767718018321-selfie-portrait-videocall_23-2149186122.avif');
 /*!40000 ALTER TABLE `file` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +61,7 @@ CREATE TABLE `file_seq` (
 
 LOCK TABLES `file_seq` WRITE;
 /*!40000 ALTER TABLE `file_seq` DISABLE KEYS */;
-INSERT INTO `file_seq` VALUES (1);
+INSERT INTO `file_seq` VALUES (151);
 /*!40000 ALTER TABLE `file_seq` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +97,7 @@ CREATE TABLE `inscription` (
 
 LOCK TABLES `inscription` WRITE;
 /*!40000 ALTER TABLE `inscription` DISABLE KEYS */;
-INSERT INTO `inscription` VALUES (1,450,'2025-11-27 14:00:00.000000','2025-11-27 14:00:00.000000','CONFIRMÉ',1,1,1),(2,750,'2025-11-28 10:30:00.000000','2025-11-28 10:30:00.000000','EN_ATTENTE',2,2,2),(3,600,'2026-01-05 09:15:00.000000','2026-01-05 09:15:00.000000','PAYÉ',3,3,3);
+INSERT INTO `inscription` VALUES (1,450,'2025-11-27 14:00:00.000000','2025-11-27 14:00:00.000000','PRESENT',1,1,1),(2,750,'2025-11-28 10:30:00.000000','2025-11-28 10:30:00.000000','EN_ATTENTE',2,2,2),(3,600,'2026-01-05 09:15:00.000000','2026-01-05 09:15:00.000000','PAY',3,3,3),(4,450,'2025-12-30 09:17:37.582000','2025-12-30 09:17:37.582000','CONFIRM',1,1,52),(8,600,'2026-02-12 17:00:00.000000','2025-12-30 09:29:49.383000','CONFIRM',3,3,52),(52,450,'2025-12-12 17:30:00.000000','2026-01-02 07:47:39.737000','CONFIRM',1,1,52),(53,450,'2025-12-12 17:30:00.000000','2026-01-05 13:00:00.462000','CONFIRM',1,1,52),(102,450,'2025-12-12 17:30:00.000000','2026-01-05 13:42:48.746000','CONFIRM',1,1,52),(103,750,'2026-01-23 16:30:00.000000','2026-01-05 13:43:33.725000','CONFIRM',2,2,52),(104,450,'2026-01-06 07:50:25.862000','2026-01-06 07:50:25.862000','CONFIRM',1,1,52);
 /*!40000 ALTER TABLE `inscription` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,7 +119,7 @@ CREATE TABLE `inscription_seq` (
 
 LOCK TABLES `inscription_seq` WRITE;
 /*!40000 ALTER TABLE `inscription_seq` DISABLE KEYS */;
-INSERT INTO `inscription_seq` VALUES (101);
+INSERT INTO `inscription_seq` VALUES (201);
 /*!40000 ALTER TABLE `inscription_seq` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +146,7 @@ CREATE TABLE `instructor` (
 
 LOCK TABLES `instructor` WRITE;
 /*!40000 ALTER TABLE `instructor` DISABLE KEYS */;
-INSERT INTO `instructor` VALUES (1,'Intervenant freelance','lodie','Martin','Développement web et pédagogie numérique'),(2,'CDI','Antoine','Leclerc','Design UX/UI et accessibilité'),(3,'Intervenant freelance','Nadia','Roux','Management de projet et méthodes agiles');
+INSERT INTO `instructor` VALUES (1,'Freelance','Maude','Martin','Développement web et pédagogie numérique'),(2,'CDI','Antoine','Leclerc','Design UX/UI et accessibilité'),(3,'Intervenant freelance','Nadia','Roux','Management de projet et méthodes agiles');
 /*!40000 ALTER TABLE `instructor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -194,7 +195,7 @@ CREATE TABLE `place` (
 
 LOCK TABLES `place` WRITE;
 /*!40000 ALTER TABLE `place` DISABLE KEYS */;
-INSERT INTO `place` VALUES (1,'12 rue de la République','Lyon',30,'69002'),(2,'45 avenue des Champs-Élysées','Paris',20,'75008'),(3,'3 boulevard Longchamp','Marseille',25,'13001');
+INSERT INTO `place` VALUES (1,'12 rue de la République','Lyon',30,'69002'),(2,'45 avenue des Champs-Élysées','Paris',20,'75008'),(3,'3 boulevard Longchamp','Marseille',25,'13001'),(52,'3 rue Emile Masson','Nantes',50,'44000'),(102,'4 allée Jean Paul Sartre','Noisiel',60,'77186');
 /*!40000 ALTER TABLE `place` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -216,7 +217,7 @@ CREATE TABLE `place_seq` (
 
 LOCK TABLES `place_seq` WRITE;
 /*!40000 ALTER TABLE `place_seq` DISABLE KEYS */;
-INSERT INTO `place_seq` VALUES (101);
+INSERT INTO `place_seq` VALUES (201);
 /*!40000 ALTER TABLE `place_seq` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -351,6 +352,7 @@ CREATE TABLE `training` (
   `prerequisites` longtext NOT NULL,
   `price` float NOT NULL,
   `title` varchar(255) NOT NULL,
+  `model_file_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -361,7 +363,7 @@ CREATE TABLE `training` (
 
 LOCK TABLES `training` WRITE;
 /*!40000 ALTER TABLE `training` DISABLE KEYS */;
-INSERT INTO `training` VALUES (1,'Informatique','Introduction aux fondamentaux du développement web : HTML, CSS et JavaScript.','Cette formation initie les participants aux technologies de base du web. Nous abordons la structure d\'une page HTML, la mise en forme avec CSS (box model, flexbox, grid) et les principes de base de JavaScript (variables, fonctions, DOM). Les sessions alternent théorie et ateliers pratiques : vous construirez une mini-application responsive de A à Z. À la fin du cours, vous serez capable de créer et déployer une page web simple, de connaître les bonnes pratiques d\'accessibilité et d\'optimisation des performances.',14,'dev-web-init.jpg',NULL,'Aucun prérequis technique nécessaire ; curiosité pour l\'informatique recommandée.',450,'Initiation au développement web'),(2,'Design','Méthodes et outils pour concevoir des interfaces centrées utilisateur.','La formation couvre le processus complet : recherche utilisateur, personas, architecture de l\'information, wireframes, prototypage et tests utilisateurs. Des cas concrets permettent de comprendre l\'importance de l\'accessibilité, de la hiérarchie visuelle et des micro-interactions. Des ateliers guident les participants de l\'idéation au prototype cliquable. Les apprenants reçoivent des retours personnalisés pour améliorer leur design.',21,'ux-ui-conception.jpg',NULL,'Connaissance de base en informatique ; intérêt pour le design.',750,'Conception UX/UI pour produits numériques'),(3,'Management','Principes et pratiques agiles pour piloter des équipes efficacement.','Cette formation explore Scrum, Kanban, les cérémonies agiles, la gestion du backlog, la planification, la rétrospective et les métriques. Grâce à des simulations de sprint et des jeux de rôles, les participants apprennent à prioriser la valeur, animer une équipe et favoriser la livraison continue. L’accent est mis sur le leadership agile, la communication et la gestion du changement dans les organisations.',16,'agile-gestion.jpg',NULL,'Exprience professionnelle recommande.',600,'Gestion de projet agile');
+INSERT INTO `training` VALUES (1,'Informatique','Introduction aux fondamentaux du développement web : HTML, CSS et JavaScript.','Cette formation initie les participants aux technologies de base du web. Nous abordons la structure d\'une page HTML, la mise en forme avec CSS (box model, flexbox, grid) et les principes de base de JavaScript (variables, fonctions, DOM). Les sessions alternent théorie et ateliers pratiques : vous construirez une mini-application responsive de A à Z. À la fin du cours, vous serez capable de créer et déployer une page web simple, de connaître les bonnes pratiques d\'accessibilité et d\'optimisation des performances.',14,'1767684132665-slider-programme-dev-web.jpeg',NULL,'Aucun prérequis technique nécessaire ; curiosité pour l\'informatique recommandée.',450,'Initiation au développement web',NULL),(2,'Design','Méthodes et outils pour concevoir des interfaces centrées utilisateur.','La formation couvre le processus complet : recherche utilisateur, personas, architecture de l\'information, wireframes, prototypage et tests utilisateurs. Des cas concrets permettent de comprendre l\'importance de l\'accessibilité, de la hiérarchie visuelle et des micro-interactions. Des ateliers guident les participants de l\'idéation au prototype cliquable. Les apprenants reçoivent des retours personnalisés pour améliorer leur design.',21,'1767684665621-101_UX_vs_UI_illustration_blog-1.png',NULL,'Connaissance de base en informatique ; intérêt pour le design.',750,'Conception UX/UI pour produits numériques',NULL),(3,'Management','Principes et pratiques agiles pour piloter des équipes efficacement.','Cette formation explore Scrum, Kanban, les cérémonies agiles, la gestion du backlog, la planification, la rétrospective et les métriques. Grâce à des simulations de sprint et des jeux de rôles, les participants apprennent à prioriser la valeur, animer une équipe et favoriser la livraison continue. L\'accent est mis sur le leadership agile, la communication et la gestion du changement dans les organisations.',16,'1767684747798-MicrosoftTeams-image-10.jpg',NULL,'Exprience professionnelle recommandée.',600,'Gestion de projet agile',NULL);
 /*!40000 ALTER TABLE `training` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -414,7 +416,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'camille.dupont@example.fr','Camille',NULL,'Dupont','MotDePasseTest123!',NULL,1),(2,'lucas.moreau@example.fr','Lucas',NULL,'Moreau','PasswordTest456!',NULL,2),(3,'sofia.bernard@example.fr','Sofia',NULL,'Bernard','Secret789!',NULL,1),(52,'matteo.baldinetti1@gmail.com','Mattéo',NULL,'Baldinetti','$2b$05$k/lfZKCa26eJ2.cN/RTIq.dxKgQT5NL4s/zOa2KWUXe7tDER9cUXi',NULL,0);
+INSERT INTO `user` VALUES (1,'camille.dupont@gmail.com','Camille','1767717969329-premium_photo-1689551670902-19b441a6afde.jpg','Dupont','$2b$05$1FKehkKfor9tAXQrG3gQdex4lXCbqxvT0W9Uu/h7qyg3WlVz/dxwa','+33695705961',0),(2,'lucas.moreau@gmail.com','Lucas','1767717915141-happy-young-man-laughing_23-2148911860.avif','Moreau','$2b$05$oIdTkuQ.K9xtJJREu.qfGuHJ1StuGmOrsm.zHpgw9Sp4ylb7vRDC2','+33749465790',0),(3,'sofia.bernard@gmail.com','Sofia','1767718018321-selfie-portrait-videocall_23-2149186122.avif','Bernard','$2b$05$hb6CzjwYXrzrMIVyPrl9S.a2zIsmwZRsW7NGlSyb7m/pVDbrqQLtK','+33632473645',1),(52,'matteo.baldinetti1@gmail.com','Mattéo',NULL,'Baldinetti','$2b$10$lZxajEYpndhj6jCH/nr4He/m9RO/M//tdXonEzqdrCbCfhwQ83gK2','',1),(102,'admin@txlforma.fr','admin','1767717058580-admin-panel.png','admin','$2b$05$aZYQiNKhJqLIRDO8jKUB/.PUdj6b11J2q14D2h9.pjcX7mO9aoHjK','+33 695061275',2);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -436,7 +438,7 @@ CREATE TABLE `user_seq` (
 
 LOCK TABLES `user_seq` WRITE;
 /*!40000 ALTER TABLE `user_seq` DISABLE KEYS */;
-INSERT INTO `user_seq` VALUES (151);
+INSERT INTO `user_seq` VALUES (201);
 /*!40000 ALTER TABLE `user_seq` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -449,4 +451,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-28 21:05:15
+-- Dump completed on 2026-01-06 16:52:54
